1c6e5e07565a554d71a147a53d62bea5eebd1654
rahmafira01 <rahmadwimaghfira@gmail.com>
2024-09-08 21:31:46 +0700

    udah sampai ceklis 6

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import show_main
-from django.urls import path, include
+from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-]
-
-urlpatterns = [
-    path('', include('main.urls')),
 ]
