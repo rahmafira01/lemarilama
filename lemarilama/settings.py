91c587fdd52bc53bc9c4f60636c6caaec3d3ad8d
rahmafira01 <rahmadwimaghfira@gmail.com>
2024-09-17 22:25:47 +0700

    Selesai Tugas Individu 3

--- lemarilama/settings.py
+++ lemarilama/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://rahma-dwi31-lemarilama.pbp.cs.ui.ac.id", "http://rahma-dwi31-lemarilama.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://rahma-dwi31-lemarilama.pbp.cs.ui.ac.id", "https://rahma-dwi31-lemarilama.pbp.cs.ui.ac.id"]
