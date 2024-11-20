d4b539dd1c7e2e64ed9b4c127ae3eb863c5d3711
rahmafira01 <rahmadwimaghfira@gmail.com>
2024-11-20 11:11:24 +0700

    yey selesai tugas individu 9

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import edit_product
 from main.views import delete_product
-from main.views import add_product_ajax
+from main.views import add_product_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('edit-product/<str:id>', edit_product, name='edit_product'),
     path('delete/<str:id>', delete_product, name='delete_product'),
     path('create-product-ajax', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
-from main.forms import ProductForm
-from main.models import Product
-from django.http import HttpResponse
-from django.core import serializers
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.http import HttpResponse
+from django.core import serializers
+from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+from main.forms import ProductForm
+from main.models import Product
+from django.shortcuts import render, reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -135,5 +137,23 @@ def add_product_ajax(request):
 
     return HttpResponse(b"CREATED", status=201)
 
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
 
+        data = json.loads(request.body)
+        new_product = Product.objects.create(
+            user=request.user,
+            name=data["name"],
+            price=int(data["price"]),
+            description=data["description"]
+        )
 
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
